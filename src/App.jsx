1a93908d51dc132ab182ae11dcfe6586cdae6077
HyunJoo Kim <hyunjoogo@gmail.com>
2022-10-13 15:44:40 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    const yoon = () => {
+        console.log("a");
+    };
+
+    return (
+        <div className="App">Hello world</div>
+    );
+}
+
+export default App;
